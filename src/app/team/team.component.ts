bffc8d6d2868ba9373d0f999b0cd03bc532ac905
KaushalShah1307 <KaushalShah1307@gmail.com>
2020-09-07 12:25:06 +0530

    cdn: loading team covers from cdn as well

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TeamComponent implements OnInit {
 
   people = [
-    { name: "Hetan Shah", image: "./assets/images/thumbnails/hetan.jpg" },
-    { name: "Chinmay Deshpande", image: "./assets/images/thumbnails/chinmayedit.jpg" }
+    { name: "Hetan Shah", image: "https://res.cloudinary.com/kaushalshah1307/image/upload/v1599461635/Friends%27%20Republic/Assets/hetan_ts24mj.jpg" },
+    { name: "Chinmay Deshpande", image: "https://res.cloudinary.com/kaushalshah1307/image/upload/v1599461631/Friends%27%20Republic/Assets/chinmayedit_w5nxix.jpg" }
   ]
 
   constructor() { }
