7617ccecb04e5a5170f65b0fde41a22ce4411983
KaushalShah1307 <KaushalShah1307@gmail.com>
2021-08-06 18:56:32 +0530

    few changes to the contact and the about sections

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  aboutus = [
+    {
+      para1: "In 2016, school buddies, Chinmay and Hetan teamed up to found Friends’ Republic - a film production company that creates music videos, documentaries, branded content and all that filmmaking goodness. The idea is to produce emotionally resonant work in a variety of formats, that cuts right through the internet noise."
+    },
+    {
+      para1: "We are here to help you and your stories move forward. Get in touch and let’s make some films together!"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
