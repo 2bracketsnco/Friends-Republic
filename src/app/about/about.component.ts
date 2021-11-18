556a24cc19b9653f3699d616d437846e7b8e6587
KaushalShah1307 <KaushalShah1307@gmail.com>
2021-11-18 14:49:58 +0530

    few more modifications to the aboutus and contactus sections with new styles to meet the design aspects

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,10 +9,19 @@ export class AboutComponent implements OnInit {
 
   aboutus = [
     {
-      para1: "In 2016, school buddies, Chinmay and Hetan teamed up to found Friends’ Republic - a film production company that creates music videos, documentaries, branded content and all that filmmaking goodness. The idea is to produce emotionally resonant work in a variety of formats."
+      para1: "In 2016, school buddies, Chinmay and Hetan teamed up to found Friends’ Republic - a Mumbai based film production company that creates music videos, documentaries, branded content and all that filmmaking goodness. The idea is to produce emotionally resonant work in a variety of formats."
     },
     {
-      para1: "We are here to help you and your stories move forward. Get in touch and let’s make some films together!"
+      para1: "We are here to help you and your stories move forward."
+    },
+    {
+      para1: ""
+    },
+    {
+      para1: ""
+    },
+    {
+      para1: "Get in touch and let’s make some films together!"
     }
   ]
 
