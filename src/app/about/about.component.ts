2bce6d6e9a90941a02779b2ec92c982717bd1df6
KaushalShah1307 <KaushalShah1307@gmail.com>
2021-11-25 17:31:14 +0530

    reconstruct the use of object for aboutUs translations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,16 +12,10 @@ export class AboutComponent implements OnInit {
       para1: "In 2016, school buddies, Chinmay and Hetan teamed up to found Friends’ Republic - a Mumbai based film production company that creates music videos, documentaries, branded content and all that filmmaking goodness. The idea is to produce emotionally resonant work in a variety of formats."
     },
     {
-      para1: "We are here to help you and your stories move forward."
+      para2: "We are here to help you and your stories move forward."
     },
     {
-      para1: ""
-    },
-    {
-      para1: ""
-    },
-    {
-      para1: "Get in touch and let’s make some films together!"
+      para3: "Get in touch and let’s make some films together!"
     }
   ]
 
