f86e2f6a77b639c3cf5fde70a8739e563e9665cd
Kaushal Shah <KaushalShah1307@gmail.com>
2023-04-12 22:54:50 +0530

    update @angular-cli packages

--- src/app/dek/dek.component.spec.ts
+++ src/app/dek/dek.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DekComponent } from './dek.component';
 
@@ -6,7 +6,7 @@ describe('DekComponent', () => {
   let component: DekComponent;
   let fixture: ComponentFixture<DekComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ DekComponent ]
     })
